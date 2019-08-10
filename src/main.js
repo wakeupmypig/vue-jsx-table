11534510205a98dc023b3466ec62cbb6fbdf0b8b
fandonglai <704072802@qq.com>
2019-08-10 10:56:48 +0800

    vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import iView from 'iview';
 import App from './App.vue';
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false;
-
+Vue.use(iView);
 new Vue({
   render: h => h(App),
 }).$mount('#app');
